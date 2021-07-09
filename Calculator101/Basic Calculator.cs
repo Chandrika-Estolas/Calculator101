05c01a62c774d11dc7e54c7395ef6690c104b6c9
Chandrika Estolas <Chandrika.estolas@gmail.com>
2021-07-09 20:36:43 +0800

    OOP CALCULATOR

---
+++ Calculator101/Basic Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator101
+{
+    public partial class Calculator : Form
+    {
+        VariablesandMethod VariablesandMethod = new VariablesandMethod();
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Num_click(object sender, EventArgs e)
+        {
+            {
+                if (VariablesandMethod.IsOperationPressed)
+                {
+                    Result_box.Text = "";
+                }
+            }
+            Button Num = (Button)sender;
+            VariablesandMethod.UserInput = Num.Text;
+            if (Result_box.Text != "0")
+                Result_box.Text += VariablesandMethod.UserInput;
+            else
+                Result_box.Text = "" + VariablesandMethod.UserInput;
+
+            VariablesandMethod.NumInput1 = VariablesandMethod.UserInput;
+            VariablesandMethod.IsOperationPressed = false;
+        }
+        private void Operation_click(object sender, EventArgs e)
+        {
+            {
+                if (VariablesandMethod.IsOperationPressed)
+                {
+                    VariablesandMethod.NumInput1 = VariablesandMethod.UserInput;
+                    Result_box.Text = "";
+                }
+            }
+            Button Operation = (Button)sender;
+            VariablesandMethod.Operation_str = (Operation.Text);
+            VariablesandMethod.NumInput1 = Result_box.Text;
+            Result_box.Text += VariablesandMethod.Operation_str;
+            Result_label.Text = VariablesandMethod.NumInput1;
+            VariablesandMethod.IsOperationPressed = true;
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            Result_box.Clear();
+            Result_label.Text = "0";
+        }
+
+        private void DEL_Click(object sender, EventArgs e)
+        {
+            if (Result_box.Text != string.Empty)
+            {
+                int Result_box_length = (Result_box.Text.Length);
+                if (Result_box_length >= 1)
+                {
+                    Result_box.Text = Result_box.Text.Remove(Result_box_length - 1);
+                }
+            }
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            VariablesandMethod.NumInput1 = Result_label.Text;
+            VariablesandMethod.NumInput2 = Result_box.Text;
+            VariablesandMethod.Calculation();
+            Result_label.Text = Convert.ToString(VariablesandMethod.Result);
+            Result_box.Text = VariablesandMethod.NumInput2;
+        }
+
+        }
+    } 
+
+
+
+
---
+++ Calculator101/VariablesandMethod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator101
+{
+    class VariablesandMethod
+    {
+        public string Operation_str;
+        public string UserInput;
+        public float Result; 
+        public float second;
+        public bool IsOperationPressed;
+
+        private string numInput1;
+        public string NumInput1
+        {
+            get
+            {
+                return numInput1;
+            }
+
+            set
+            {
+                numInput1 = value;
+            }
+        }
+
+        private string numInput2;
+
+        public string NumInput2
+        {
+            get
+            {
+                return numInput2;
+            }
+
+            set
+            {
+                numInput2 = value;
+            }
+        }
+
+        public void Calculation()
+        {
+            second = float.Parse(NumInput2);
+
+                if (Operation_str == "x")
+                    Result = float.Parse(NumInput1) * (second);
+                NumInput2 = string.Empty + Result.ToString();
+
+                if (Operation_str == "/")
+                {
+                    if (second == 0)
+                    {
+                        NumInput2 = "Undefined";
+                    }
+                    else
+                    {
+                        Result = float.Parse(NumInput1) / (second);
+                        NumInput2 = string.Empty + Result.ToString();
+                    }
+                }
+
+                if (Operation_str == "+")
+                    Result = float.Parse(NumInput1) + (second);
+                NumInput2 = string.Empty + Result.ToString();
+
+                if (Operation_str == "-")
+                    Result = float.Parse(NumInput1) - (second);
+                NumInput2 = string.Empty + Result.ToString();
+            
+        }
+    }
+}
